75e623209bd1daae89838ca294bcd58c73f98b56
Daniel <DanielSenpai95@gmail.com>
2018-01-22 14:16:55 +0100

    Variablen und Funktionsnamen angepasst, neue Funktionen hinzugefügt.

--- CRUD.js
+++ CRUD.js
@@ -4,148 +4,268 @@
  * Autor: Peter Dick
  * Seit: 19.01.2017
  */
-openDB = function() {
-    const sqlite3 = require('sqlite3').verbose();
+
+
+  // Referenzen einbinden.
+ const logger = require('./logger');
+ const sqlite3 = require('sqlite3');
+
+
+ // Zu exportierende Objekte definieren.
+ module.exports = {
+   initDatabase: initDatabase,
+   insertKey: insertKey,
+   getIP: getIP,
+   registerIP: registerIP,
+   unregisterIP: unregisterIP,
+   getLastRegisteredIP: getLastRegisteredIP
+ }
+
+
+function openDB() {
     let db = new sqlite3.Database('./WebDB.db', (err) => {
         if (err) {
-            return console.error(err.message);
+            return console.error("Fehler: " + err.message);
         }
-        console.log('Connected to the Web database.');
+        console.log('Datenbankverbindung hergestellt.');
     });
     return db;
 }
 
-closeDB = function(db) {
+
+function closeDB(db) {
     db.close((err) => {
         if (err) {
 	    console.log('error');
-            return console.error(err.message);
+            return console.error("Fehler: " + err.message);
         }
-        console.log('Close the database connection.');
+        console.log('Datenbankverbindung geschloßen.');
     });
 }
 
-exports.insertSchluessel = function(codewort) {
+
+function initDatabase() {
+  let db = openDB();
+  console.log('Erstelle Tabelle Key.');
+  db.run('CREATE TABLE IF NOT EXISTS Key ( id INTEGER NOT NULL,'
+         +'codeword TEXT NOT NULL, expiration_date TEXT NOT NULL,'
+         +'PRIMARY KEY (id))');
+  console.log('Erstelle Tabelle Device.')
+  db.run('CREATE TABLE IF NOT EXISTS Device ( id INTEGER NOT NULL,'
+         +' name TEXT NOT NULL UNIQUE, ip_address TEXT NOT NULL,'
+         +' key_id INTEGER NOT NULL, FOREIGN KEY(key_id) REFERENCES Key(id),'
+         +' PRIMARY KEY (id) )');
+  console.log('Tabellen erstellt.')
+  closeDB(db);
+}
+
+function insertNewKey(codeword) {
     let db = openDB();
-    let sql = 'INSERT INTO Schluessel(Codewort, Ablaufdatum) VALUES(?, datetime("now"))';
-    db.run(sql, [codewort], function(err) {
+    let sql = 'INSERT INTO Key(codeword, expiration_date) VALUES(?, datetime("now"))';
+    db.run(sql, [codeword], function(err) {
         if(err) {
-            return console.error(err.message);
+            return console.error("Fehler: " + err.message);
         }
-        console.log('Schlüssel eingefügt');
+        console.log('Schlüssel eingefügt.');
     });
     closeDB(db);
 }
 
-exports.insertGeraet = function(name, ipAdr, schluesselID) {
+function insertNewDevice(name, ipAdr, keyid) {
     let db = openDB();
-    let sql = 'INSERT INTO Geraet(Name, IPAdresse, SchluesselID) VALUES(?, ?, ?)';
-    db.run(sql, [name, ipAdr, schluesselID], function(err) {
+    let sql = 'INSERT INTO Device(name, ip_address, key_id) VALUES(?, ?, ?)';
+    db.run(sql, [name, ipAdr, keyid], function(err) {
         if(err) {
-            return console.error(err.message);
+            return console.error("Fehler: " + err.message);
         }
         console.log('Gerät eingefügt');
     });
     closeDB(db);
 }
 
-exports.deleteGeraetID = function(id) {
+
+function deleteDeviceByID(id) {
     let db = openDB();
-    let sql = 'DELETE FROM Geraet WHERE ID = ?';
+    let sql = 'DELETE FROM Device WHERE id = ?';
     db.run(sql, [id], function(err) {
         if(err) {
-            return console.error(err.message);
+            return console.error("Fehler: " + err.message);
         }
-        console.log('Geraet gelüscht');
+        console.log('Device gelöscht');
     });
     closeDB(db);
 }
 
-exports.deleteGeraetName = function(name) {
+
+function deleteDeviceByName(name) {
     let db = openDB();
-    let sql = 'DELETE FROM Geraet WHERE Name = ?';
+    let sql = 'DELETE FROM Device WHERE name = ?';
     db.run(sql, [name], function(err) {
         if(err) {
-            return console.error(err.message);
+            return console.error("Fehler: " + err.message);
         }
-        console.log('Geraet gelüscht');
+        console.log('Device gelöscht');
     });
     closeDB(db);
 }
 
-exports.deleteSchluesselID = function(id) {
+
+function deleteKeyByID(id) {
     let db = openDB();
-    let sql = 'DELETE FROM Schluessel WHERE ID = ?';
+    let sql = 'DELETE FROM Key WHERE id = ?';
     db.run(sql, [id], function(err) {
         if(err) {
-            return console.error(err.message);
+            return console.error("Fehler: " + err.message);
         }
-        console.log('Schlüssel gelüscht');
+        console.log('Schlüssel gelöscht');
     });
     closeDB(db);
 }
 
-exports.selectGeraetID = function(id, callback) {
+
+function deleteKeyByCodeword(codeword) {
     let db = openDB();
-    let sql = 'SELECT * FROM Geraet WHERE ID = ?';
-    db.get(sql, [id], function(err, rows) {
+    let sql = 'DELETE FROM Key WHERE codeword = ?';
+    db.run(sql, [codeword], function(err) {
         if(err) {
-            return console.error(err.message);
+            return console.error("Fehler: " + err.message);
+        }
+        console.log('Schlüssel gelöscht');
+    });
+    closeDB(db);
+}
+
+
+function selectAllDevices(callback) {
+    let db = openDB();
+    let sql = 'SELECT * FROM Device';
+    db.get(sql, function(err, rows) {
+        if(err) {
+            return console.error("Fehler: " + err.message);
         }
         callback(rows);
     });
     closeDB(db);
 }
 
-exports.selectGeraetName = function(name, callback) {
+
+function selectDeviceByID(id, callback) {
     let db = openDB();
-    let sql = 'SELECT * FROM Geraet WHERE Name = ?';
+    let sql = 'SELECT * FROM Device WHERE id = ?';
+    db.get(sql, [id], function(err, rows) {
+        if(err) {
+            return console.error("Fehler: " + err.message);
+        }
+        callback(rows);
+    });
+    closeDB(db);
+}
+
+
+function selectDeviceByName(name, callback) {
+    let db = openDB();
+    let sql = 'SELECT * FROM Device WHERE name = ?';
     db.get(sql,[name], function(err, rows) {
         if(err) {
-            return console.error(err.message);
+            return console.error("Fehler: " + err.message);
         }
         callback(rows);
     });
     closeDB(db);
 }
 
-exports.selectSchluesselID = function(id, callback) {
+
+function selectDeviceByKeyID(keyID, callback) {
     let db = openDB();
-    let sql = 'SELECT * FROM Schluessel WHERE ID = ?';
+    let sql = 'SELECT * FROM Device WHERE key_id = ?';
+    db.get(sql,[keyID], function(err, rows) {
+        if(err) {
+            return console.error("Fehler: " + err.message);
+        }
+        callback(rows);
+    });
+    closeDB(db);
+}
+
+
+function selectAllKeys(callback) {
+    let db = openDB();
+    let sql = 'SELECT * FROM Key';
     db.get(sql, [id], function(err, rows) {
         if(err) {
-            return console.error(err.message);
+            return console.error("Fehler: " + err.message);
         }
         callback(rows);
     });
     closeDB(db);
 }
 
-exports.updateSchluessel = function(id, updateSchluessel, newCodewort) {
-    let sql;
-    if(updateSchluessel) {
-        sql = 'UPDATE Schluessel SET Codewort = ?, Ablaufdatum = datetime("now") WHERE ID = ?';
-    } else {
-        sql = 'UPDATE Schluessel SET Codewort = ? WHERE ID = ?';
-    }
+
+function selectKeyByID(id, callback) {
     let db = openDB();
-    db.run(sql, [newCodewort, id], function(err) {
+    let sql = 'SELECT * FROM Key WHERE id = ?';
+    db.get(sql, [id], function(err, rows) {
         if(err) {
-            return console.error(err.message);
+            return console.error("Fehler: " + err.message);
         }
-        console.log('Row(s) updated: ' + this.changes);
+        callback(rows);
     });
     closeDB(db);
 }
 
-exports.updateGeraet = function(id, newName, newIP, newSchluesselID) {
-    let sql = 'UPDATE Geraet SET Name = ?, IPAdresse = ?, SchluesselID = ? WHERE ID = ?';
+
+function selectKeyByCodeword(codeword, callback) {
     let db = openDB();
-    db.run(sql, [newName, newIP, newSchluesselID, id], function(err) {
+    let sql = 'SELECT * FROM Key WHERE codeword = ?';
+    db.get(sql, [codeword], function(err, rows) {
         if(err) {
-            return console.error(err.message);
+            return console.error("Fehler: " + err.message);
         }
-        console.log('Row(s) updated: ' + this.changes);
+        callback(rows);
+    });
+    closeDB(db);
+}
+
+
+function updateKeyCodeword(id, updateKey, newCodeword) {
+    let sql;
+    if(updateKey) {
+        sql = 'UPDATE Key SET codeword = ?, expiration_date = datetime("now") WHERE id = ?';
+    } else {
+        sql = 'UPDATE Key SET codeword = ? WHERE id = ?';
+    }
+    let db = openDB();
+    db.run(sql, [newCodeword, id], function(err) {
+        if(err) {
+            return console.error("Fehler: " + err.message);
+        }
+        console.log('Zeilen aktualisiert: ' + this.changes);
+    });
+    closeDB(db);
+}
+
+
+function updateDeviceKeyID(id, newName, newIP, newKeyid) {
+    let sql = 'UPDATE Device SET name = ?, ip_address = ?, key_id = ? WHERE id = ?';
+    let db = openDB();
+    db.run(sql, [newName, newIP, newKeyid, id], function(err) {
+        if(err) {
+            return console.error("Fehler: " + err.message);
+        }
+        console.log('Zeilen aktualisiert: ' + this.changes);
+    });
+    closeDB(db);
+}
+
+
+function updateDeviceKeyID(id, newKeyid) {
+    let sql = 'UPDATE Device SET key_id = ? WHERE id = ?';
+    let db = openDB();
+    db.run(sql, [newKeyid, id], function(err) {
+        if(err) {
+            return console.error("Fehler: " + err.message);
+        }
+        console.log('Zeilen aktualisiert: ' + this.changes);
     });
     closeDB(db);
 }
